ca23d4cba3ecb9433bda15da75a96fead3072317
paulohfin <paulohfin@yahoo.com.br>
2017-06-21 17:01:15 -0300

    Add files via upload

---
+++ configuracao.php
@@ -0,0 +1,33 @@
+<!--Configuração do acesso ao banco de dados e das variáveis, no define, e dos caminhos que precisam ser instanciadas como variáveis-->
+
+<?php
+
+/* O nome do banco de dados*/
+define('DB_NAME', 'id1917107_ikacalc');
+
+/* Usuário do banco de dados MySQL */
+define('DB_USER', 'id1917107_root');
+
+/* Senha do banco de dados MySQL */
+define('DB_PASSWORD', 'ikacalc123');
+
+/* Nome do host do MySQL */
+define('DB_HOST', 'localhost');
+
+/* Caminho absoluto para a pasta do sistema **/
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+	
+/* Caminho no server para o sistema **/
+if ( !defined('BASEURL') )
+	define('BASEURL', '/ikacalc/');
+	
+/* Caminho do arquivo de banco de dados na própria máquina*/
+if ( !defined('DBAPI') )
+	define('DBAPI', ABSPATH . 'inc/banco.php');
+
+/*Definindo arquivos de cabeçalho e rodapé justamente para não ficar copiando código*/
+define('CABECALHO_TEMPLATE', ABSPATH . 'inc/cabecalho.php');
+define('RODAPE_TEMPLATE', ABSPATH . 'inc/rodape.php');
+
+?>
